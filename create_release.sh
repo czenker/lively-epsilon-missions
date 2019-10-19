db38c93c55ad07e318a2be3202054ab873da1029
Christian Zenker <dev@xopn.de>
2019-10-19 17:38:39 +0200

    fix release script to include all necessary files

--- create_release.sh
+++ create_release.sh
@@ -6,9 +6,9 @@ BUILD_DIR="_build/"
 
 echo "Building version ${VERSION}..."
 
-ZIPFILE="$(pwd)/release-${VERSION}.zip"
-GZFILE="$(pwd)/release-${VERSION}.tar.gz"
-XZFILE="$(pwd)/release-${VERSION}.tar.xz"
+ZIPFILE="$(pwd)/lively-epsilon-missions-${VERSION}.zip"
+GZFILE="$(pwd)/lively-epsilon-missions-${VERSION}.tar.gz"
+XZFILE="$(pwd)/lively-epsilon-missions-${VERSION}.tar.xz"
 
 rm -rf _build $ZIPFILE $GZFILE $XZFILE
 mkdir -p _build
@@ -20,7 +20,11 @@ cp -r lively_epsilon/src/ "_build/lively_epsilon/src"
 cp lively_epsilon/Readme.md lively_epsilon/LICENSE lively_epsilon/init.lua "_build/lively_epsilon/"
 
 cp -r 01_krepios/ _build/01_krepios/
-cp scenario_01_krepios.lua Readme.md LICENSE factionInfo.lua shipTemplates.lua _build/
+cp -r names/ _build/names/
+mkdir _build/docs
+cp docs/*.adoc _build/docs
+cp -r docs/_build _build/docs/html
+cp scenario_01_krepios.lua Readme.md LICENSE factionInfo.lua shipTemplates.lua Changelog.md _build/
 
 cd _build
 
