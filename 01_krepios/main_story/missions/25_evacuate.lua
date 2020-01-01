54dcc57a668e89cd3a473d0b722d26c1adcb7723
Christian Zenker <dev@xopn.de>
2020-01-01 09:05:09 +0100

    give evac mission even when fortress is destroyed before all other stations

--- 01_krepios/main_story/missions/25_evacuate.lua
+++ 01_krepios/main_story/missions/25_evacuate.lua
@@ -1,16 +1,26 @@
 local t = My.Translator.translate
 local mission
 
+local function giveMissionIfNotExists()
+    if mission == nil then
+        mission = Mission:new({})
+        Mission:withBroker(mission, t("story_mission_evacuate"))
+        mission:setHint(t("story_mission_evacuate_hint", My.Wormhole:getSectorName()))
+        Mission:forPlayer(mission)
+        mission:setPlayer(My.World.player)
+        mission:setMissionBroker(My.World.colonel)
+        mission:accept()
+        mission:start()
+        My.World.player:addMission(mission)
+    end
+end
+
 My.EventHandler:register("onClosingInToFortress", function()
-    mission = Mission:new({})
-    Mission:withBroker(mission, t("story_mission_evacuate"))
-    mission:setHint(t("story_mission_evacuate_hint", My.Wormhole:getSectorName()))
-    Mission:forPlayer(mission)
-    mission:setPlayer(My.World.player)
-    mission:setMissionBroker(My.World.colonel)
-    mission:accept()
-    mission:start()
-    My.World.player:addMission(mission)
+    giveMissionIfNotExists()
+end)
+
+My.EventHandler:register("onCommanderDead", function()
+    giveMissionIfNotExists()
 end)
 
 My.EventHandler:register("onEnemiesDestroyed", function()
