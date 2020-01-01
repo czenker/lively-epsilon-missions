68ed4c5a7c7d645c440c8007e9aafe030f87bd2b
Christian Zenker <dev@xopn.de>
2020-01-01 09:14:16 +0100

    add model_data and sfx to releases

--- create_release.sh
+++ create_release.sh
@@ -22,10 +22,11 @@ cp lively_epsilon/Readme.md lively_epsilon/LICENSE lively_epsilon/init.lua "_bui
 cp -r 01_krepios/ _build/01_krepios/
 cp -r names/ _build/names/
 cp -r lang/ _build/lang/
+cp -r resources/ _build/resources/
 mkdir _build/docs
 cp docs/*.adoc _build/docs
 cp -r docs/_build _build/docs/html
-cp scenario_01_krepios.lua Readme.md LICENSE factionInfo.lua shipTemplates.lua Changelog.md _build/
+cp scenario_01_krepios.lua Readme.md LICENSE factionInfo.lua shipTemplates.lua model_data.lua Changelog.md _build/
 
 cd _build
 
